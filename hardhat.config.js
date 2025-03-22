9c0a0e69c581bcbb7e404eb2b7090cf577ee399d
Aditya Prakash <adityaprakashiu@gmail.com>
2025-03-22 20:59:48 +0530

    Removed Truffle files, updated tests, and added Hardhat configuration

---
+++ hardhat.config.js
@@ -0,0 +1,17 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+module.exports = {
+  solidity: "0.8.28", // Solidity version
+  defaultNetwork: "hardhat", // Default network for testing
+  networks: {
+    // Local Hardhat network
+    hardhat: {
+      chainId: 31337, // Hardhat's default chain ID
+    },
+    // Localhost network (for running a node locally)
+    localhost: {
+      url: "http://127.0.0.1:8545", // Local node URL
+      chainId: 31337, // Same as Hardhat
+    },
+  },
+};
